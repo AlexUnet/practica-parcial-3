28e713cb6358c17b5b2417257f6612765305b66c
AlexUnet <alejandro.lopezp@unet.edu.ve>
2021-03-08 00:40:10 -0400

    Sing up completo
    el formulario se envía correctamente falta la validación de datos se
    agregó la pantalla de salida.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,48 +4,37 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/user/", name="user")
      */
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        return $this->render('user/index.html.twig');
     }
 
-    public function formSingUpAction(Request $request){
-        $name= '';
-        $lastName = '';
-        $password = '';
-        $mail = '';
-        $phone = 0;
-
-        return $this->render('user/index.html.twig',['name' => $name,]);
-    }
-
-    public function createUser(): Response{
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $user = new User();
+    /**
+     * @Route("/user/create", name="createUser", methods="POST")
+     * 
+     */
+    public function create(){
+        
+        $user = new User();              
         $user->setIsAdmin(false);
-        $user->setName('admin');
-        $user->setLastName('First');
-        $user->setPassword('admin');
-        $user->setMail('admin@gmail.com');
-        $user->setPhoneNumber('9122890');
+        $user->setName($_POST['name']);
+        $user->setLastName($_POST['lastName']);
+        $user->setPassword($_POST['password']);
+        $user->setMail($_POST['mail']);
+        $user->setPhoneNumber($_POST['phone']);
 
-
+        $entityManager = $this->getDoctrine()->getManager();  
         $entityManager->persist($user);
-
         $entityManager->flush();
 
-        return new Response('Saved new user with id'.$user->getId());
+        return $this->render('user/create.html.twig');
     }
 }
