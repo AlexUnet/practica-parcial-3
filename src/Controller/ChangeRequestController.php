89cb5f1c290f3d118adbdfbecaaa7aaf53037c8a
AlexUnet <alejandro.lopezp@unet.edu.ve>
2021-03-10 18:43:15 -0400

    Change Request view (admin/user)
    in user shows list of its requests and a button for making a
    new request, for admin show list of all requests and a button
    to process the request

--- src/Controller/ChangeRequestController.php
+++ src/Controller/ChangeRequestController.php
@@ -10,25 +10,41 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ChangeRequestController extends AbstractController
 {
+
     /**
      * @Route("/change/request", name="change_request")
      */
     public function index(): Response
     {
-        session_start();
+        if (!isset($_SESSION)) {
+            session_start();
+        }
         $changeList = $this->buildRequestsList($_SESSION['user']->getIsAdmin());
-        if($_SESSION['user']->getIsAdmin())
-            return $this->render('change_request/changeRequestsAdmin.html.twig',['changeRequestList' => $changeList]);
+        if ($_SESSION['user']->getIsAdmin())
+            return $this->render('change_request/changeRequestsAdmin.html.twig', ['changeRequestList' => $changeList]);
         else
-            return $this->render('change_request/changeRequest.html.twig',['changeRequestList' => $changeList]);
+            return $this->render('change_request/changeRequest.html.twig', ['changeRequestList' => $changeList]);
     }
 
-    public function buildRequestsList($admin){
-        if($admin)
-            return $this->getDoctrine()->
-                getRepository(ChangeRequest::class)->findAll();
+    public function buildRequestsList($admin)
+    {
+        if ($admin)
+            return $this->getDoctrine()->getRepository(ChangeRequest::class)->findAll();
         else
-            return $this->getDoctrine()->
-                getRepository(User::class)->findOneBy(['id' => $_SESSION['user']->getId()])->getChangeRequest();
+            return $this->getDoctrine()->getRepository(User::class)->findOneBy(['id' => $_SESSION['user']->getId()])->getChangeRequest();
+    }
+
+    /**
+     * @Route("/change/state", methods="GET")
+     */
+    public function setRequestState()
+    {
+        $request = $this->getDoctrine()->getRepository(ChangeRequest::class)->findOneBy(['id' => $_GET['requestId']]);
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['id' => $_GET['userId']]);
+        //$package = $user->getPackage();
+        //$newPackage = $request->getPackage();
+        //$user->setPackage($newPackage);
+        $request->setState(false);
+        return $this->index();        
     }
 }
